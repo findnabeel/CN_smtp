594b6f9c349271e9f8d7675a81a2844addd39ef4
findnabeel <findnabeel@gmail.com>
2020-10-16 10:35:21 +0400

    Add files via upload

--- solution.py
+++ solution.py
@@ -29,6 +29,12 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
 
     # Send MAIL FROM command and print server response.
     # Fill in start
+    sendtext = 'MAIL FROM:mtn2@nyu.edu\r\n'
+    clientSocket.send(sendtext.encode())
+    recv1 = clientSocket.recv(1024).decode()
+    print(recv1)
+    if recv1[:3] != '250':
+        print('250 reply not received from server.')
     # Fill in end
 
     # Send RCPT TO command and print server response.
